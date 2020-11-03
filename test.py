b40beb3b881b789bfb0583374c1721213310a123
martinyuan886 <nulizhu_86@hotmail.com>
2020-11-03 16:26:53 +0800

    add patch to simulate update config result

--- test.py
+++ test.py
@@ -9,6 +9,12 @@ class Item(BaseModel):
     is_offer: bool = None
 
 
+class Item_patch(BaseModel):
+    name: str
+    is_ok: bool
+
+
+
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
@@ -24,3 +30,13 @@ def read_item(item_id: int, q: str = None):
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
     return {"item_name": item.name, "item_id": item_id}
+
+
+#patch can use below curl command to check
+#curl -X PUT "http://127.0.0.1:8000/items/2" -H  "accept: application/json" -H  "Content-Type: application/json" -d "{\"name\":\"test\",\"price\":10,\"is_offer\":true}"
+@app.patch("/items/{item_id}")
+def update_item(item_id: int, item: Item_patch):
+    return {"item_id": item_id, "item_name": item.name, "ok_or_not": item.is_ok}
+
+
+#add for test auto-reload
