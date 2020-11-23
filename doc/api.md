69a8c4a87d553f296303b651c8753440afa73698
martinyuan886 <nulizhu_86@hotmail.com>
2020-11-23 16:25:33 +0800

    add docs and continue to update test.py

--- doc/api.md
+++ doc/api.md
@@ -1,31 +1,202 @@
 
+## 2.3 AUTO接收sn获取结果反馈
+
+### 2.3.1 功能描述
+
+Agent尝试ssh设备并获取设备sn后，通过该接口反馈AUTO获取结果
+
+### 2.3.2 请求说明
+
+> 请求方式： POST
+>
+> 请求URL： /api/v1/resources/net_devices/get_sn_result
+
+### 2.3.3 HTTP 头定义
+
+|     字段     |       参数       |
+| :----------: | :--------------: |
+| Content-Type | application/json |
+| accept       | application/json |
+
+### 2.3.4 请求参数
+
+|         字段         | 类型 | 是否必填 | 说明           |
+| :------------------: | :--: | :------: | -------------- |
+| result             | string |    是    | 任务ID         |
+| net_port_info      | list  |    是    | 测试需要的信息   |
+| dev_info           | list  |    是    |测试需要的信息    |
+| template_info      | list  |    是    | 测试需要的信息   |
 
 
 
-###retrieve fw info with vendor & sn info
-#code
-@app.get("/api/v1/resources/fws")
-def read_item(vendor: str = None, sn: str = None):
-    return {"dev": "FW", "vendor": vendor, "sn": sn}
+### 2.3.6 返回结果
 
-#Curl
-curl -X GET "http://47.95.4.161:31070/api/v1/resources/fws?vendor=h3c&sn=123abc" -H  "accept: application/json"
+| 字段      | 类型   | 说明                               |
+| --------- | ------ | ---------------------------------- |
+| id        | int    | 任务ID                             |
+| uuid      | string | 任务UUID                           |
+| done      | bool   | 是否完成测试                       |
+| name      | str    | 测试名称                           |
+| user_uuid | str    | 用户uuid                           |
+| running   | bool   | 测试是否正在执行，false为 未在执行 |
 
-#request url
-http://47.95.4.161:31070/api/v1/resources/fws?vendor=h3c&sn=123abc
+### 2.3.7 状态码
 
-#response code
-200
+- 正确返回状态码
 
-#response body
+  | 状态码 |   说明   |
+  | :----: | :------: |
+  |  200   | 正常返回 |
+
+  
+
+- 错误状态码
+
+  | 状态码 |             说明              |
+  | :----: | :---------------------------: |
+  |  401   | 表示用户没有权限（token错误） |
+  |  404   |    Task instance not found    |
+  |  400   |   Task instance is running    |
+  |  400   |    Task instance has done     |
+
+
+### 2.3.5 请求/返回示例
+#### agent成功获取SN且auto在库中匹配到sn
+- request
+```
+请求url: http://x.x.x.x:xxxx/api/v1/resources/net_devices/get_sn_result
+方法:post
+请求内容（json）：
 {
-  "dev": "FW",
-  "vendor": "h3c",
-  "sn": "123abc"
+  "result": "success",
+  "failure_info": "N/A",
+  "dev_info": {
+    "sn": "123",
+    "dhcp_ip": "192.168.33.10",
+    "vendor": "H3C",
+    "device_type": "FW"
+  }
 }
 
-#response header
- content-length: 41 
- content-type: application/json 
- date: Wed11 Nov 2020 07:58:55 GMT 
- server: uvicorn 
+```
+
+- response
+```json
+{
+  "sn_match_result": "success",
+  "dev_config_data": {
+    "hostname": "NFV-SRV-1",
+    "mng_ip": "10.10.10.1",
+    "netmask": "24",
+    "gw": "10.10.10.254"
+  }
+}
+```
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+## 2.3 AUTO接收sn获取结果反馈
+
+### 2.3.1 功能描述
+
+Agent尝试ssh设备并获取设备sn后，通过该接口反馈AUTO获取结果
+
+### 2.3.2 请求说明
+
+> 请求方式： POST
+>
+> 请求URL： /api/v1/resources/net_devices/get_sn_result
+
+### 2.3.3 HTTP 头定义
+
+|     字段     |       参数       |
+| :----------: | :--------------: |
+| Content-Type | application/json |
+| accept       | application/json |
+
+### 2.3.4 请求参数
+
+|         字段         | 类型 | 是否必填 | 说明           |
+| :------------------: | :--: | :------: | -------------- |
+| 请求头中的<task-id>  | int   |    是    | 任务ID         |
+| net_port_info      | list  |    是    | 测试需要的信息   |
+| dev_info           | list  |    是    |测试需要的信息    |
+| template_info      | list  |    是    | 测试需要的信息   |
+
+### 2.3.5 请求示例
+```
+请求url: http://x.x.x.x:xxxx/api/v1/resources/net_devices/get_sn_result
+方法:post
+请求内容（json）：
+{
+  "result": "success",
+  "failure_info": "N/A",
+  "dev_info": {
+    "sn": "123",
+    "dhcp_ip": "192.168.33.10",
+    "vendor": "H3C",
+    "device_type": "FW"
+  }
+}
+
+```
+
+
+
+### 2.3.6 返回结果
+
+| 字段      | 类型   | 说明                               |
+| --------- | ------ | ---------------------------------- |
+| id        | int    | 任务ID                             |
+| uuid      | string | 任务UUID                           |
+| done      | bool   | 是否完成测试                       |
+| name      | str    | 测试名称                           |
+| user_uuid | str    | 用户uuid                           |
+| running   | bool   | 测试是否正在执行，false为 未在执行 |
+
+### 2.3.7 状态码
+
+- 正确返回状态码
+
+  | 状态码 |   说明   |
+  | :----: | :------: |
+  |  200   | 正常返回 |
+
+  
+
+- 错误状态码
+
+  | 状态码 |             说明              |
+  | :----: | :---------------------------: |
+  |  401   | 表示用户没有权限（token错误） |
+  |  404   |    Task instance not found    |
+  |  400   |   Task instance is running    |
+  |  400   |    Task instance has done     |
+
+### 2.3.8 返回示例
+
+```json
+{
+  "sn_match_result": "success",
+  "dev_config_data": {
+    "hostname": "NFV-SRV-1",
+    "mng_ip": "10.10.10.1",
+    "netmask": "24",
+    "gw": "10.10.10.254"
+  }
+}
+```
+
+
--- test.py
+++ test.py
@@ -16,49 +16,49 @@ app = FastAPI()
 def read_root():
     return "Hi, this is auto api simulator"
 
-#下面函参中
-# -item_id为client在url中输入的
-# -q为在url中?后面带的如/items/1?q=haha，此处使用python类型声明声明为字符串，None代表不是必填
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
-
-
-#retrieve fw info with vendor & sn info
-#linux cmd commmand:
-#curl -v http://auto.cmri.com:31070/api/v1/resources/fws?vendor=h3c\&sn=123ab    #在linux下此处&得用\转义一下
-@app.get("/api/v1/resources/fws")
-def read_item(vendor: str = None, sn: str = None):
-    return {"dev": "FW", "vendor": vendor, "sn": sn}
-
-
-
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: bool = None
-
-
-#put can use below curl command to check
-#curl -X PUT "http://127.0.0.1:8000/items/2" -H  "accept: application/json" -H  "Content-Type: application/json" -d "{\"name\":\"test\",\"price\":10,\"is_offer\":true}"
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
-
-
-
-class Item_patch(BaseModel):
-    name: str
-    is_ok: bool
-
-
-
-#patch can use below curl command to check
-#curl -X PATCH "http://47.95.4.161:31070/items/1" -H  "accept: application/json" -H  "Content-Type: application/json" -d "{\"name\":\"string\",\"is_ok\":true}"
-@app.patch("/items/{item_id}")
-def update_item(item_id: int, item: Item_patch):
-    return {"item_id": item_id, "item_name": item.name, "ok_or_not": item.is_ok}
-    #print("no return")
+##下面函参中
+## -item_id为client在url中输入的
+## -q为在url中?后面带的如/items/1?q=haha，此处使用python类型声明声明为字符串，None代表不是必填
+#@app.get("/items/{item_id}")
+#def read_item(item_id: int, q: str = None):
+#    return {"item_id": item_id, "q": q}
+#
+#
+##retrieve fw info with vendor & sn info
+##linux cmd commmand:
+##curl -v http://auto.cmri.com:31070/api/v1/resources/fws?vendor=h3c\&sn=123ab    #在linux下此处&得用\转义一下
+#@app.get("/api/v1/resources/fws")
+#def read_item(vendor: str = None, sn: str = None):
+#    return {"dev": "FW", "vendor": vendor, "sn": sn}
+#
+#
+#
+#class Item(BaseModel):
+#    name: str
+#    price: float
+#    is_offer: bool = None
+#
+#
+##put can use below curl command to check
+##curl -X PUT "http://127.0.0.1:8000/items/2" -H  "accept: application/json" -H  "Content-Type: application/json" -d "{\"name\":\"test\",\"price\":10,\"is_offer\":true}"
+#@app.put("/items/{item_id}")
+#def update_item(item_id: int, item: Item):
+#    return {"item_name": item.name, "item_id": item_id}
+#
+#
+#
+#class Item_patch(BaseModel):
+#    name: str
+#    is_ok: bool
+#
+#
+#
+##patch can use below curl command to check
+##curl -X PATCH "http://47.95.4.161:31070/items/1" -H  "accept: application/json" -H  "Content-Type: application/json" -d "{\"name\":\"string\",\"is_ok\":true}"
+#@app.patch("/items/{item_id}")
+#def update_item(item_id: int, item: Item_patch):
+#    return {"item_id": item_id, "item_name": item.name, "ok_or_not": item.is_ok}
+#    #print("no return")
 
 
 class Result(str, Enum):
@@ -97,6 +97,12 @@ class Item_get_sn_result(BaseModel):
     
 
 
+###response
+#200: (1)get sn success(sn found), dev_config_info returned   (2)get sn failed, return "got it"
+#404：sn not found in db
+#500: internal error, let sn == "internal_error" to simulate
+
+
 sn_list = ["123", "456", "789"]       #simulate sn in db, if sn replied by agent isn't in this list, corresponding error info would be replied 
 
 #post can use below curl command to check
@@ -110,12 +116,23 @@ def update_get_sn_result(item: Item_get_sn_result):
     if get_sn_result == "failed":
         failure_info_get_sn = item.failure_info
         print("agent failed to get sn, failure info is: {}".format(failure_info_get_sn))
+        return {"detail": "Failed result got by AUTO"}
     elif get_sn_result == "success":
         sn_get = item.dev_info.sn
-        if sn_get in sn_list:
-            return {"sn_match_result": "success", "dev_config_data":{"hostname": "NFV-SRV-1", "mng_ip": "10.10.10.1", "netmask": "24","gw": "10.10.10.254"}}
-        else:
-            print("sn got was not found in db, please further check")
-            #return {"sn_match_result": "failed", }
-            raise HTTPException(status_code=404, detail="sn got was not found in db")
-
+        db_response = retrieve_dev_info_by_sn_from_db(sn_get)   #fake db operation func, replace with real db operation soon
+        return db_response
+        
+        
+def retrieve_dev_info_by_sn_from_db(sn_get: str):
+    if sn_get == "internal_error":
+        try:
+            print ("for simulate db operation error")
+            raise Exception
+        except:
+            raise HTTPException(status_code=500, detail="internal error")
+    elif sn_get in sn_list:
+        return {"sn_match_result": "success", "dev_config_data":{"hostname": "NFV-SRV-1", "mng_ip": "10.10.10.1", "netmask": "24","gw": "10.10.10.254"}}  #user dict to simualte record in db table soon
+    else:
+        print("sn got was not found in db, please further check")
+        #return {"sn_match_result": "failed", }
+        raise HTTPException(status_code=404, detail="sn got was not found in db")
